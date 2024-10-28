f5470c09763ad97a8655613805b96fe69c393b73
martin bamber <mbamber1986@gmail.com>
2024-10-28 23:52:07 +0000

    removed Session Core Moved Everything back to Sessions, Added Datetime Back to Sessions.php & Rewrote Codebase

--- src/Sessions.php
+++ src/Sessions.php
@@ -2,19 +2,22 @@
 
 namespace LazarusPhp\SessionManager;
 
-class Sessions extends SessionCore
+use LazarusPhp\DatabaseManager\Database;
+use LazarusPhp\DateManager\Date;
+use PDO;
+
+class Sessions extends Database
 {
 
-    public function setTz($tz="Europe/London")
-    {
-        return date_default_timezone_set($tz);
-    }
 
-    public function getTz()
-    {
-        return date_default_timezone_get();
-    }
+    public int $expiry = 1;
+    public string $format = "y-m-d H:i:s";
+    private $table = "sessions";
+    public $date;
+    private $writer;
     
+
+    // Magic Methods to control Sessions.
     public function __set($name, $value)
     {
         $_SESSION[$name] = $value;
@@ -28,30 +31,6 @@ class Sessions extends SessionCore
         }
     }
 
-    public function deleteSessions(...$args): void
-    {
-        $count = count($args);
-        echo $count;
-        if($count > 0)
-        {
-            foreach($args as $arg)
-            {
-                if(array_key_exists($arg,$_SESSION))
-                {
-                    echo $arg;
-                    unset($_SESSION[$arg]);
-                    echo "$arg Deleted";
-                }
-            }
-        }
-        else
-        {
-            session_destroy();
-            echo "We Will just delete All Sessions";
-        }
-       
-    }
-
     public function __isset($name)
     {
         return isset($_SESSION[$name]);
@@ -61,13 +40,108 @@ class Sessions extends SessionCore
     {
         unset($_SESSION[$name]);
     }
+    
+    // End Magic Methods
+    
+    // Assignment Properties
 
-    public function listall()
+    public function setExpiry(int $expiry):void
     {
-        foreach($_SESSION as $key => $sessions)
-        {
-            echo "$key = $sessions <br>";
+        $this->expiry = $expiry;
+    }
+
+    public function setFormat(string $format):void
+    {
+        $this->format = $format;
+    }
+
+    public function setTable($table)
+    {
+        $this->table = $table;
+    }
+
+
+    // End Assignment Properties
+    
+    // Generate Session handler
+    
+    private function generateSession():void
+    {
+        session_set_save_handler(
+            [$this,"open"],
+            [$this,"close"],
+            [$this,"read"],
+            [$this,"write"],
+            [$this,"destroy"],
+            [$this,"gc"],
+        );
+    }
+
+    public function start(): void
+    {
+        $this->date = Date::withAddedTime("now","P".$this->expiry."D");
+        // Include the sessionHandler
+        $this->generateSession();
+        // End Session Handler
+
+        // Detect Status of session
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            // Load session_start
+            if (session_start()) {
+                // Set the cookie to keep session active between the database and the browser
+                setcookie(session_name(), session_id(), Date::toTimestamp($this->date), "/", "." . $_SERVER['HTTP_HOST']);
+            }
         }
     }
 
+    
+    // Session Handler Methods;
+    public function open():bool
+    {
+       return true;
+    }
+
+
+    public function read($sessionID) :mixed
+    {
+
+        $stmt = $this->sql("SELECT * FROM ".$this->table." WHERE session_id = :sessionID",[":sessionID"=>$sessionID])
+        ->One(PDO::FETCH_ASSOC);
+        return $stmt ? $stmt["data"] : ""; 
+    }
+
+    public function write($sessionID, $data): bool
+    {
+        $params =  [":sessionID"=>$sessionID,":data"=>$data,":expiry"=>$this->date->format($this->format)];
+        $this->GenerateQuery("REPLACE INTO " . $this->table . " (session_id,data,expiry) VALUES(:sessionID,:data,:expiry)",$params);
+        return true;
+       
+    }
+
+    public function close(): bool
+    {
+        return true;
+    }
+
+    public function destroy($sessionID): bool
+    {
+        $params = [":sessionID"=>$sessionID];
+        $this->GenerateQuery("DELETE FROM " . $this->table . " WHERE session_id=:sessionID",$params);
+        return true;
+    }
+
+    public function gc()
+    {
+        $expiry = Date::create("now");
+
+        try {
+            $params = [":expiry"=>$expiry];
+            $this->GenerateQuery("DELETE FROM sessions WHERE expiry  < :expiry",$params);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage() . $e->getCode());
+        }
+
+        unset($_COOKIE[session_name()]);
+    }
+    // End Session handler Methods
 }
