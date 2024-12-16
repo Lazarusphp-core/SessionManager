161ebf66bd062c1ac989eff6da24db2f729d224d
mbamber1986 <mbamber1986@gmail.com>
2024-12-16 16:22:14 +0000

    Added Set Rules for Session Writer

--- src/Interfaces/SessionControl.php
+++ src/Interfaces/SessionControl.php
@@ -3,4 +3,10 @@ namespace LazarusPhp\SessionManager\Interfaces;
 
 interface SessionControl
 {
+
+    public function customBoot():void;
+    public function readQuery(string $sessionID):mixed;
+    public function writeQuery(string $sesionId,string|int $data):bool;
+    public function destroyQuery($sesionId):bool;
+    public function gcQuery():void;
 }
