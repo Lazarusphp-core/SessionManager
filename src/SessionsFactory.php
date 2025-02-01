c39c0f89ed650b32f89520840abf21cfd00bef26
martin bamber <mbamber1986@gmail.com>
2025-02-01 23:56:55 +0000

    Added Session Factory Class to give the option of avoing Writing new instantiations Everytime

---
+++ src/SessionsFactory.php
@@ -0,0 +1,33 @@
+<?php
+namespace LazarusPhp\SessionManager;
+
+class SessionsFactory
+{
+    public static function create()
+    {
+        return new Sessions();
+    }
+
+    public static function instantiate(array $classname,array $config = [])
+    {
+        return (new Sessions())->init($classname, $config);
+    }
+
+    public static function set($key,$value)
+    {
+        $session = new Sessions();
+        $session->$key = $value;
+    }
+
+    public static function get($key)
+    {
+        $session = new Sessions();
+        return $session->$key;
+    }
+
+    public static function destroy($key)
+    {
+        $session = new Sessions();
+        unset($session->$key);
+    }
+}
