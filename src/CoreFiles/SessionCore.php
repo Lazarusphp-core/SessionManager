91907b03d3dc080ca695bd5a2d099aaa45b891cd
martin bamber <mbamber1986@gmail.com>
2025-04-12 23:44:59 +0100

    Created new SessionCore Php Adds More Requirements and checks

---
+++ src/CoreFiles/SessionCore.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace LazarusPhp\SessionManager\CoreFiles;
+
+class SessionCore
+{
+    private $errors = [];
+    private $config = [];
+
+    public function setConfig(array $config)
+    {
+        $count = count($config);
+        $keys = array_keys($config);
+        $supportedKeys= ["secure","days","table","lifetime","sameSite","httpOnly"];
+        
+        if(count($config) >= 1)
+        {
+           
+            $this->getUnsupportedKeys($keys,$supportedKeys);
+
+            if(count($this->errors) >= 1)
+            {
+            
+            }
+            else
+            {
+                foreach($config as $key => $value)
+                {
+                    
+                    if(!array_key_exists($key,$this->config)){
+                        $this->config[$key] = $value;
+                        }
+                }
+            }
+        }
+        else
+        {
+            $this->config = ["days" => 7,"table" => "sessions"];
+        }
+        return $this->config;
+    }
+
+    private function doCount($input,$value,?string $operator=null)
+    {
+        $operator = $operator ?? "=";
+        return (count($input) . $operator . $value) ? true : false;
+    }
+    private function getUnsupportedKeys($keys,$supportedKeys)
+    {
+        $unsupported = array_diff($keys,$supportedKeys);
+        if($unsupported)
+        {
+            foreach($unsupported as $unsupported)
+            {
+                $this->errors[] = "Error Found with Key : $unsupported is not supported";
+            }
+        }
+    }
+
+    private function returnConfig(string $key,?string $altvalue=null)
+    {
+        if(isset($this->config[$key]))
+        {
+            $this->config[$key] = $altvalue ?? $this->config[$key];
+        }
+        return $this->config[$key];
+    }
+
+}
